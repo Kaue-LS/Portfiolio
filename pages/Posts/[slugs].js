66cb184642656d42cb56713ed2671df4a5956d81
Kauê Leite <79471947+Kaue-LS@users.noreply.github.com>
2022-03-08 02:25:45 -0300

    Update [slugs].js

--- pages/Posts/[slugs].js
+++ pages/Posts/[slugs].js
@@ -1,41 +1,41 @@
-// import { Head } from "next/document";
-// import { useRouter } from "next/router";
-// import PostBody from "../../Components/project/ProjectBody";
-// import PostHeader from "../../Components/project/ProjectHead";
+import { Head } from "next/document";
+import { useRouter } from "next/router";
+import PostBody from "../../Components/project/ProjectBody";
+import PostHeader from "../../Components/project/ProjectHead";
 // import {getAllPosts,getPostBySlug} from '../../Lib/Projects'
 // import markdownToHtml from '../../Lib/markdownToHtml'
-// export default function Posts ({post,morePosts,preview}){
-//     const router = useRouter()
-//     if(!router.isFallback && !post?.slug){
-//         return 'Deu Ruim'
-//     }
+export default function Posts ({post,morePosts,preview}){
+    const router = useRouter()
+    if(!router.isFallback && !post?.slug){
+        return 'Deu Ruim'
+    }
 
-//     return(
-//         <div>
-//             {router.isFallback?(
-//                 <h1>Carregando</h1>
-//             ):(
-//                 <>
-//                 <article>
-//                     <Head>
-//                         <title>
-//                             {post.title }| Blog Next Portfólio
-//                         </title>
-//                         <meta property="og:image"></meta>
-//                     </Head>
-//                     <PostHeader
-//                     title={post.title}
-//                     coverImage={post.coverImage}
-//                     date={post.date}
-//                     author={post.author}
-//                     />
-//                     <PostBody content={post.content}/>
-//                 </article>
-//                 </>
-//             )}
-//         </div>
-//     )
-// }
+    return(
+        <div>
+            {router.isFallback?(
+                <h1>Carregando</h1>
+            ):(
+                <>
+                <article>
+                    <Head>
+                        <title>
+                            {post.title }| Blog Next Portfólio
+                        </title>
+                        <meta property="og:image"></meta>
+                    </Head>
+                    <PostHeader
+                    title={post.title}
+                    coverImage={post.coverImage}
+                    date={post.date}
+                    author={post.author}
+                    />
+                    <PostBody content={post.content}/>
+                </article>
+                </>
+            )}
+        </div>
+    )
+}
 // export async function getStaticProps({ params }) {
 //   const post = getPostBySlug(params.slug, [
 //     'title',
