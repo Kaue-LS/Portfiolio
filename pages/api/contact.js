c6fee2f142db9ee49820e54871a290c21098055b
Kauê Santos <kaue.ls0506@gmail.com>
2022-04-23 23:19:13 -0300

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,30 +1,30 @@
-const nodemailer=require('nodemailer')
-const config= require('../../config.local')
-export default function SendEmail(req,res){
-    let transporter = nodemailer.createTransport({
-        service:'gmail',
+// const nodemailer=require('nodemailer')
+// const config= require('../../config.local')
+// export default function SendEmail(req,res){
+//     let transporter = nodemailer.createTransport({
+//         service:'gmail',
     
-        auth:{
-            user:config.USERMAIL,
-            pass:config.PASSMAIL
-        }
-    })
+//         auth:{
+//             user:config.USERMAIL,
+//             pass:config.PASSMAIL
+//         }
+//     })
 
-    let mailOptions ={
-        from: `"Fred Foo 👻" <${config.USERMAIL}>`, // sender address
-        to: config.USERMAIL,
-        replyTo:req.body.email, // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: req.body.message, // plain text body
-    }
+//     let mailOptions ={
+//         from: `"Fred Foo 👻" <${config.USERMAIL}>`, // sender address
+//         to: config.USERMAIL,
+//         replyTo:req.body.email, // list of receivers
+//         subject: "Hello ✔", // Subject line
+//         text: req.body.message, // plain text body
+//     }
 
-    transporter.sendMail(mailOptions,(err,data)=>{
-        if(err){
-            console.log('Erro Occurs',err)
-        }else{
-            console.log('Email sent!!',data)
-        }
-    })
+//     transporter.sendMail(mailOptions,(err,data)=>{
+//         if(err){
+//             console.log('Erro Occurs',err)
+//         }else{
+//             console.log('Email sent!!',data)
+//         }
+//     })
     // transporter.sendMail({
     //     from: `"Fred Foo 👻" <${config.USERMAIL}>`, // sender address
     //     to: config.USERMAIL,
@@ -37,4 +37,4 @@ export default function SendEmail(req,res){
       
     // })
     // .catch((error)=>{res.send(error)})
-}
+// }
