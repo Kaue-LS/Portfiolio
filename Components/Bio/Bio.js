4f24995a487e38ca9cc03e2c0e710de6a1a2426e
Kauê Santos <kaue.ls0506@gmail.com>
2022-04-23 23:45:37 -0300

    Update Bio.js

--- Components/Bio/Bio.js
+++ Components/Bio/Bio.js
@@ -8,16 +8,16 @@ export default function Bio() {
             Meu nome é <strong>Kauê Leite</strong>, nasci moro na cidade de <strong>São Paulo</strong> na zona
             leste, gosto de ficar sempre perto de dispositivos eletrônicos como
             consoles e computadores, tanto para jogar videogames como também
-            para programar,  pois, se eu não me irritar com chefão difícil, eu
+            para programar, pois, se não me irritar com chefão difícil,
             fico com raiva pelo código não está funcionando.</p>
             <p> 
             Formado no curso técnico de <strong>Análise e Desenvolvimento de Sistemas na ETEC Centro
             Paula Souza de Ilha Solteira</strong>, foi onde comecei a me interessar na
             área de programação, aprendendo sobre banco de dados, análise de
-            sistemas, programação web, lógica de programação e também design. 
+            sistemas, programação web, lógica de programação e também design.
             Hoje em dia estou estudando na <strong>Blue Edtech</strong>, um curso online de
             programação, me ajudou a aprofundar mais a fundo sobre
-            desenvolvimento Front-end,  sendo a área onde estou procurando meu
+            desenvolvimento Front-end, sendo a área onde estou procurando meu
             primeiro emprego.
           </p>
         </S.Paragraph>
